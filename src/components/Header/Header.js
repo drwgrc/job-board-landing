266ffac58b6970577c9c8112a8fc1d8e3f9113cf
Andrew Garcia <andrew@drwgrc.dev>
2022-03-26 23:44:57 -0400

    fixed import error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import styles from './Header.module.scss';
 import { ReactComponent as Logo } from './../../assets/icons/logo.svg';
-import Navbar from '../../src/components/Navbar/Navbar';
+import Navbar from './../Navbar/Navbar';
 import { ReactComponent as MenuIcon } from './../../assets/icons/menu.svg';
-import AccountButtons from '../../src/components/AccountButtons/AccountButtons';
+import AccountButtons from './../AccountButtons/AccountButtons';
 
 const Header = () => {
   return (
